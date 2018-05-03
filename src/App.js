8358e645535be0306e065b35211945fd2031628e
David MacKintosh <dgawmackintosh@gmail.com>
2018-05-03 12:44:37 +0100

    Makes guest list dynamic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import GuestList from './GuestList.js';
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Denise',
+        isConfirmed: false,
+      },
+      {
+        name: 'Stewart',
+        isConfirmed: true,
+      },
+      {
+        name: 'Lucy',
+        isConfirmed: true,
+      }
+    ]
+  };
+
+  getTotalInvited = () => this.state.guests.length;
+
+  // getAttendingGuests = () => {}
+  // getUnconfirmedGuests = () => {}
 
   render() {
     return (
       <div className="App">
         <header>
           <h1>RSVP</h1>
-          <p>A Treehouse App</p>
+          <p>Who will be there?</p>
           <form>
               <input type="text" value="Safia" placeholder="Invite Someone" />
               <button type="submit" name="submit" value="submit">Submit</button>
@@ -36,36 +59,11 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          <GuestList guests={this.state.guests} />
         </div>
       </div>
     );
   }
-}
+};
 
 export default App;
